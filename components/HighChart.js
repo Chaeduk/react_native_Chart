d45f66796c3f187dcac8276df04ada4f2f3f33e5
Chaeduk <kkj2551@naver.com>
2022-03-30 15:01:43 +0900

    [reafactor] map -> reduce

--- components/HighChart.js
+++ components/HighChart.js
@@ -4,46 +4,48 @@ import ChartView from 'react-native-highcharts';
 import dummy from '../data/dummy.json';
 import moment from 'moment';
 
-const botData = data => {
-  const result = data.map(function (cur, index) {
-    return {x: moment(cur.REG_DT).toDate().getTime(), y: cur.TRADE_RETURN};
-  });
-  return result;
-};
-
-const userData = data => {
-  const result = data.map(function (cur, index) {
-    const refine = cur.TRADE_PRICE.replace(new RegExp(/,/gi), '');
-    return {
-      x: moment(cur.REG_DT).toDate().getTime(),
-      y: Number(refine),
-    };
-  });
+const splitData = data => {
+  const result = data.reduce(
+    function (acc, cur) {
+      const day = moment(cur.REG_DT);
+      if (day.hours() === 8) {
+        const refine = cur.TRADE_PRICE.replace(new RegExp(/,/gi), '');
+        acc.user = [
+          ...acc.user,
+          {x: day.toDate().getTime(), y: cur.TRADE_RETURN},
+        ];
+        acc.bot = [...acc.bot, {x: day.toDate().getTime(), y: Number(refine)}];
+      }
+      return acc;
+    },
+    {bot: [], user: []},
+  );
   return result;
 };
 
 const sixMonthSort = data => {
   const result = data.filter(function (cur) {
-    return cur.x.isBetween(moment().subtract(6, 'months'), moment());
+    return moment(cur.x).isBetween(moment().subtract(6, 'months'), moment());
   });
   return result;
 };
 
 const oneMonthSort = data => {
   const result = data.filter(function (cur) {
-    return cur.x.isBetween(moment().subtract(1, 'months'), moment());
+    return moment(cur.x).isBetween(moment().subtract(1, 'months'), moment());
   });
   return result;
 };
 
 const threeMonthSort = data => {
   const result = data.filter(function (cur) {
-    return cur.x.isBetween(moment().subtract(3, 'months'), moment());
+    return moment(cur.x).isBetween(moment().subtract(3, 'months'), moment());
   });
   return result;
 };
 
 const HighChart = () => {
+  const {bot, user} = splitData(dummy);
   const Highcharts = 'Highcharts';
   const conf = {
     title: {
@@ -87,12 +89,12 @@ const HighChart = () => {
       {
         name: 'Random',
         yAxis: 0,
-        data: botData(dummy),
+        data: threeMonthSort(bot),
       },
       {
         name: 'dd',
         yAxis: 1,
-        data: userData(dummy),
+        data: threeMonthSort(user),
       },
     ],
   };
